390e96d540ddd4a23de2dbcbd3df1461f1e2c563
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-10 18:15:51 -0700

    content is stable

--- test/content.js
+++ test/content.js
@@ -1,5 +1,9 @@
 'use strict';
-const assert = require('chai').assert;
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const should = chai.should();
+const path = require('path');
 const Content = require('../lib/helpers/content');
 
 const mixins = {
@@ -12,6 +16,8 @@ const mixins = {
     delete parent[key];
   }
 };
+
+const CONTENT_DIRECTORY = path.join(__dirname, '/content/');
 
 describe('Content', () => {
   it('should be an object', () => {
@@ -63,7 +69,7 @@ describe('Content', () => {
       assert.equal(`${obj.one.two.a} ${obj.one.two.b}`, 'hello world');
     });
 
-    it('should have be able to support mixins that use transform and mixins arguments', () => {
+    it('should be able to support mixins that use transform and mixins arguments', () => {
       const obj = {
         one: {
           two: {
@@ -79,5 +85,57 @@ describe('Content', () => {
       assert.equal(obj.one.two.foo, 'bar');
       assert.equal(obj.one.two.stay, 'here');
     });
+  });
+
+  describe('.parseFile', () => {
+    it('should be a function', () => {
+      assert.typeOf(Content.parseFile, 'function');
+    });
+
+    it('should be able to parse a cson file', () => {
+      const result = Content.parseFile(CONTENT_DIRECTORY, 'homepage.cson');
+
+      assert.typeOf(result, 'object');
+      assert.equal(result.title, 'Homepage');
+    });
+
+    it('should be able to parse a json file', () => {
+      const result = Content.parseFile(CONTENT_DIRECTORY, 'about.json');
+
+      assert.typeOf(result, 'object');
+      assert.equal(result.title, 'About');
+    });
+
+    it('should be able to parse multiple files', () => {
+      const result = Content.parseFile(CONTENT_DIRECTORY, [
+        'global.cson',
+        'about.json'
+      ]);
+
+      assert.typeOf(result, 'object');
+      assert.equal(result.title, 'About');
+      assert.equal(result.brandName, 'Expressful');
+    });
+
+    it('should use the default __extend mixin', () => {
+      const result = Content.parseFile(CONTENT_DIRECTORY, 'contact.cson');
+
+      assert.typeOf(result, 'object');
+      assert.equal(result.title, 'Contact');
+      assert.equal(result.brandName, 'Expressful');
+    });
+
+    it('should throw an error when file does not exist', () => {
+      expect(
+        Content.parseFile.bind(this, CONTENT_DIRECTORY, 'not-existing-file.cson')
+      ).to.Throw();
+    });
+
+    it('should use the default __list: directory: "name" mixin', () => {
+      const result = Content.parseFile(CONTENT_DIRECTORY, '/blog.cson');
+
+      assert.equal(result.posts.length, 2);
+    });
+
   });
 });
