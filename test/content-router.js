6f69b8ad4c5c62fed175caa9cc92b2c40fdc5490
Jeroen Ransijn <jssrdesign@gmail.com>
2016-07-26 21:03:40 -0700

    fixed content router

--- test/content-router.js
+++ test/content-router.js
@@ -1,5 +1,6 @@
 'use strict';
 const chai = require('chai');
+const express = require('express');
 const assert = chai.assert;
 const expect = chai.expect;
 const should = chai.should();
@@ -7,22 +8,14 @@ const path = require('path');
 const contentRouter = require('../lib/helpers/content-router');
 
 const CONTENT_DIRECTORY = path.join(__dirname, '/content');
+const VIEWS_DIRECTORY = path.join(__dirname, '/views');
 
 describe('contentRouter', () => {
   it('should be a function', () => {
     assert.typeOf(contentRouter, 'function');
   });
 
-  it('should walk a directory', () => {
-    contentRouter(CONTENT_DIRECTORY, (route) => {
-      assert.typeOf(route, 'object');
-      assert.typeOf(route.path, 'string');
-      assert.typeOf(route.relativeFilePath, 'string');
-      assert.typeOf(route.filePath, 'string');
-
-      return (req, res, next) => {
-        res.json({ hello: 'world' });
-      }
-    });
+  it('should return a router with a title', () => {
+    assert.equal(contentRouter(CONTENT_DIRECTORY, VIEWS_DIRECTORY).title, 'Content Routes');
   });
 });
